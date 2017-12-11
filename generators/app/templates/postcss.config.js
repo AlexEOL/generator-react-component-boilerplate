191ace602e2a54f8c494b9f98785317dd411b0d9
Alexander Melnik <Alexander.melnik90@yandex.ru>
2017-12-12 01:41:18 +0300

    Добавил postcss config файл

---
+++ generators/app/templates/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: 'sugarss',
+  plugins: {
+    csswring: {},
+    'postcss-smart-import': {},
+    autoprefixer: {
+      browsers: ['last 2 versions'],
+    },
+    'rucksack-css': {},
+  },
+};
